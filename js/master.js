22b19b5f76857259c289d58c0be8e32bf0b3adec
李毅 <thomas@23-6.site>
2020-07-27 04:09:13 +0900

    初始界面编写完成

--- js/master.js
+++ js/master.js
@@ -1,5 +1,22 @@
 $(function () {
 
+  // 随滚动屏幕显隐导航
+  $('.masthead').visibility({
+    once: false,
+    onBottomPassed: function () {
+      $('.fixed.menu').transition('fade in');
+    },
+    onBottomPassedReverse: function () {
+      $('.fixed.menu').transition('fade out');
+    }
+  });
+
+  // 创建侧边栏并追加菜单项
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+  // 初始化下拉菜单
+  $('.ui.dropdown').dropdown();
+
   console.log("[OK] 页面初始化完成");
-  
+
 });
